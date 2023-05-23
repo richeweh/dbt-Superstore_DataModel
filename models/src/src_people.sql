c5fbd2075521deac83bf17d81eda92795658dd54
Richmond Eweh <richmondeweh@ymail.com>
2023-05-23 21:11:45 +0000

    Make and initialize src_people and src_returns models from raw tables

---
+++ models/src/src_people.sql
@@ -0,0 +1,7 @@
+WITH src_people AS (
+    SELECT*
+    FROM dbtmockproject.Sample_Superstore_Dataset.Superstore_People
+)
+
+SELECT*
+FROM src_people
---
+++ models/src/src_returns.sql
@@ -0,0 +1,7 @@
+WITH src_returns AS (
+    SELECT*
+    FROM dbtmockproject.Sample_Superstore_Dataset.Superstore_Returns
+)
+
+SELECT*
+FROM src_returns
